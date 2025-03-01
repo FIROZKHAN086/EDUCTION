7dfa798b2877e0e2a63465ac9950c5da402fe25d
FIROZ KHAN <computerpc096@gmail.com>
2025-03-01 15:10:50 -0800

    devlp...

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
         },
         {
             "name":"Results",
-            "link":"/results"
+            "link":"/result"
         },
         {
             "name":"Jobs",
